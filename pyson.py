0a83b787f9cc2b63a3cfb464d7a7a22d81f2f667
Daryn Luce <daryn.luce@gmail.com>
2019-07-29 19:46:08 -0500

    added highest scaling, and lowest method

--- pyson.py
+++ pyson.py
@@ -36,29 +36,69 @@ def stat(stat,val):
 def levelStat(level,base,per):
     return (level * per) + base
     
-def highest(data,stat):
+def highest(data,stat,level=1):
     hold = 0
     champ = ''
+    perlevel = stat + 'perlevel'
+    if stat == 'movespeed' or stat == 'attackrange':
+        bolperlevel = True
+    else:
+        bolperlevel = False
     for champion in data:
-        if data[champion]['stats'][stat] > hold:
-            hold = data[champion]['stats'][stat]
-            champ = champion
-    print('{} has the highest {} at level 1, with {}'.format(champ,stat,hold))
+        if not(bolperlevel):
+            if (data[champion]['stats'][stat] +(level * data[champion]['stats'][perlevel])) > hold:
+                hold = data[champion]['stats'][stat] +(level * data[champion]['stats'][perlevel])
+                champ = champion
+                
+        else:
+            if data[champion]['stats'][stat] > hold:
+                hold = data[champion]['stats'][stat]
+                champ = champion
+                print('here')
+  
+    return champ,stat,hold
+    
+def lowest(data,stat,level=1):
+    hold = 1000
+    champ = ''
+    perlevel = stat + 'perlevel'
+    if stat == 'movespeed' or stat == 'attackrange':
+        bolperlevel = True
+    else:
+        bolperlevel = False
+    for champion in data:
+        if not(bolperlevel):
+            if (data[champion]['stats'][stat] +(level * data[champion]['stats'][perlevel])) < hold:
+                hold = data[champion]['stats'][stat] +(level * data[champion]['stats'][perlevel])
+                champ = champion
+                
+        else:
+            if data[champion]['stats'][stat] < hold:
+                hold = data[champion]['stats'][stat]
+                champ = champion
+                print('here')
+  
+    return champ,stat,hold
+
+    
 #getAllChamp(data) #List all champions
 
-level = 3 #change any level
+level = 1 #change any level
 champion = returnChamp(data, 'draven') # change any champion name
 champStats = returnStat(champion)
 name = champ(champion,'name') # list of values below
+hname,hstat,hval = highest(data,'armor',level)
+lname,lstat,lval = lowest(data,'armor',level)
 ad = [stat(champStats,'attackdamage'),stat(champStats,'attackdamageperlevel')] # list of values below
 totalad = levelStat(level,ad[0],ad[1])
+# print('-----------------------------')
+# print('{} - {}'.format(name,champ(champion,'blurb')))
+# print('-----------------------------')
+# print('{} has {} attack damage (+{} per level) base. At level {}, {} has {}'.format(name, ad[0],ad[1],level,name,totalad))
+# print('-----------------------------')
 print('-----------------------------')
-print('{} - {}'.format(name,champ(champion,'blurb')))
-print('-----------------------------')
-print('{} has {} attack damage (+{} per level) base. At level {}, {} has {}'.format(name, ad[0],ad[1],level,name,totalad))
-print('-----------------------------')
-print('-----------------------------')
-highest(data,'hp')
+print('{} has the highest base {} at level {} reaching {}'.format(hname,hstat,level,hval))
+print('{} has the lowest base {} at level {} reaching {}'.format(lname,lstat,level,lval))
 
 ### values for champ(champion, value) method
  # version
